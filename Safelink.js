abebf2fab44f0b73fada10ab8767268dfc466f37
Fasvient <59753987+Fasvient@users.noreply.github.com>
2020-02-09 16:52:09 +0700

    Update Safelink.js

--- Safelink.js
+++ Safelink.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "http://URL-TUJUAN.blogspot.com/p/poi.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://www.facebook.com/frypan24" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
